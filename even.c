392e82a194408701c3a35bd99b15680663532969
positivecse <47621096+positivecse@users.noreply.github.com>
2019-02-21 10:40:10 +0530

    Create even.c

---
+++ even.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main()
+{
+    int a;
+    printf("INPUT: ");
+    scanf("%d", &a);
+    if (a%2=0)
+        printf("EVEN");
+    else
+        printf("ODD");
+    return 0;
+}
